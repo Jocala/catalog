ac79b132eeb7fc22d4b889214b1a3c31215dfab1
jocala <jocala@jocala.com>
2026-09-25 09:18:59 -0400

    catalog: version 1.03

--- qt/package-catalog-macos.sh
+++ qt/package-catalog-macos.sh
@@ -20,7 +20,7 @@ $CPACK --config "$BUILD_DIR/CPackConfig.cmake" \
   -B "$BUILD_DIR/packages"
 
 NOTARY_PROFILE="notary-jeff"
-DMG="$BUILD_DIR/packages/catalog-1.02-Darwin.dmg"
+DMG="$BUILD_DIR/packages/catalog-1.03-Darwin.dmg"
 xcrun notarytool submit "$DMG" --keychain-profile "$NOTARY_PROFILE" --wait
 xcrun stapler staple "$DMG"
 xcrun stapler validate "$DMG"
--- qt/version.h
+++ qt/version.h
@@ -4,4 +4,4 @@
 // with CMake project(VERSION) and packaging/catalog.iss.in (#VERSION).
 #include <QString>
 
-const QString kCatalogVersion = "1.02";
+const QString kCatalogVersion = "1.03";
